bbe1db605d3f6ac7686ebe201a6775bc3e950a42
Justin Wheeler <justinwwheeler@gmail.com>
2022-01-15 22:55:44 +0000

    bug fix, sytax error on users routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-  resources :users, only [:new, :create]
+  resources :users, only: [:new, :create]
 
 end
