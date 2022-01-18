253fd336f018464705382563dde2ecb554ff405f
Justin Wheeler <justinwwheeler@gmail.com>
2022-01-18 04:44:34 +0000

    add current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
@@ -28,5 +27,10 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 
 end
