7d76f199ade60f679d3c39dcba8b486d31a39e5e
Justin Wheeler <justinwwheeler@gmail.com>
2022-01-26 05:18:53 +0000

    add more robust checks to product tests

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -8,29 +8,34 @@ RSpec.describe Product, type: :model do
       @category = Category.create(name: "Food")
       @product = Product.create(name: "Burger", price: 200, quantity: 1, category: @category)
       expect(@product.errors.full_messages).to eq([])
+      expect(@product).to be_valid
     end
 
     it "provides an error message when name is nil" do
       @category = Category.create(name: "Food")
       @product = Product.create(name: nil, price: 200, quantity: 1, category: @category)
-      expect(@product.errors.full_messages[0]).to_not be_nil
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+      expect(@product).to_not be_valid
     end
 
     it "provides an error message when price is nil" do
       @category = Category.create(name: "Food")
       @product = Product.create(name: "Burger", price: nil, quantity: 1, category: @category)
-      expect(@product.errors.full_messages[0]).to_not be_nil
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+      expect(@product).to_not be_valid
     end
 
     it "provides an error message when quantity is nil" do
       @category = Category.create(name: "Food")
       @product = Product.create(name: "Burger", price: 200, quantity: nil, category: @category)
-      expect(@product.errors.full_messages[0]).to_not be_nil
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      expect(@product).to_not be_valid
     end
 
     it "provides an error message when category is nil" do
       @product = Product.create(name: "Burger", price: 200, quantity: 1, category: nil)
-      expect(@product.errors.full_messages.length).to eq(1)
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+      expect(@product).to_not be_valid
     end
     
   end
