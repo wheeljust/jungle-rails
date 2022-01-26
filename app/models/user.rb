9c791233c4818e8619400a8565864c5db2679ddb
Justin Wheeler <justinwwheeler@gmail.com>
2022-01-26 06:00:11 +0000

    add validations for unique email and password min length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  before_validation :all_lowercase
   validates :first_name, :last_name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true, case_sensitive: false
+  validates :password, length: { minimum: 6 }
 
+  
+  private
+
+  def all_lowercase
+    self.email = email.downcase if email.present?
+  end
+  
 end
