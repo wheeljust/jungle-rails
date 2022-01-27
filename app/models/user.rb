ce4b3167770d8d77af3cf0d88e77003220f0ed2c
Justin Wheeler <justinwwheeler@gmail.com>
2022-01-27 03:27:34 +0000

    add method for authenticate credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,20 @@ class User < ActiveRecord::Base
   before_validation :all_lowercase
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true, case_sensitive: false
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 8 }
 
-  
+
+  def authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email.downcase.strip)
+    
+    if @user && @user.authenticate(password)
+      return @user
+    end
+
+    nil
+  end
+
+
   private
 
   def all_lowercase
